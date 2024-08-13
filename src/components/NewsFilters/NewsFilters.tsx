d56a3125c80ba80cb16dc56670655ce5dff4d117
Ausare <asain_baitasov@mail.ru>
2024-08-13 21:00:36 +0600

    add redux

--- src/components/NewsFilters/NewsFilters.tsx
+++ src/components/NewsFilters/NewsFilters.tsx
@@ -1,7 +1,8 @@
-import { getCategories } from '../../api/apiNews';
 import { useTheme } from '../../context/ThemeContext.js';
-import { useFetch } from '../../helpers/hooks/useFetch';
-import { CategoriesApiResponse, IFilters } from '../../interfaces';
+import { IFilters } from '../../interfaces';
+import { useAppDispatch } from '../../store/index.js';
+import { useGetCategoriesQuery } from '../../store/services/newsApi.js';
+import { setFilters } from '../../store/slices/newsSlice.js';
 import Categories from '../Categories/Categories';
 import Search from '../Search/Search';
 import Slider from '../Slider/Slider.jsx';
@@ -9,31 +10,31 @@ import styles from './styles.module.css';
 
 interface Props {
 	filters: IFilters;
-	changeFilter: (key: string, value: string | number | null) => void;
-	
 }
 
-const NewsFilters = ({ filters, changeFilter }: Props) => {
+const NewsFilters = ({ filters }: Props) => {
 	const { isDark } = useTheme();
-	const { data: dataCategories } = useFetch<CategoriesApiResponse, null>(
-		getCategories
-	);
+	const { data} = useGetCategoriesQuery(null);
+		const dispatch = useAppDispatch()
 	return (
 		<div className={styles.filters}>
-			{dataCategories ? (
+			{data ? (
 				<Slider isDark={isDark}>
 					<Categories
-						categories={dataCategories.categories}
+						categories={data.categories}
 						selectedCategory={filters.category}
-						setSelectedCategory={category => changeFilter('category', category)}
+						setSelectedCategory={category =>
+							dispatch(setFilters({ key: 'category', value: category }))
+						}
 					/>
 				</Slider>
 			) : null}
 
 			<Search
 				keywords={filters.keywords}
-				setKeywords={keywords => changeFilter('keywords', keywords)}
-				
+				setKeywords={keywords =>
+					dispatch(setFilters({ key: 'keywords', value: keywords }))
+				}
 			/>
 		</div>
 	);
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ThemeProvider } from './context/ThemeContext.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ThemeProvider } from './context/ThemeContext.tsx';
+import { store } from './store/index.ts';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider>
-
-    <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<ThemeProvider>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</ThemeProvider>
+	</React.StrictMode>
+);
